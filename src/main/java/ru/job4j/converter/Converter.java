43ed7dfa9096daf51e2db5196569e16de4862b23
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2023-09-01 13:12:41 +0300

    0. Что такое тестирование [#159431 #441432]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,21 +2,21 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        return value / 50;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        return value / 30;
+        return value / 60;
     }
 
     public static void main(String[] args) {
-        float ruble = 150;
+        float ruble = 140;
         float out = Converter.rubleToEuro(ruble);
-        float expected = 3f;
+        float expected = 2f;
         boolean passed = expected == out;
         System.out.println(ruble + " rubles are 3 euro. Test result : " + passed);
         out = Converter.rubleToDollar((ruble));
-        expected = 5f;
+        expected = 2.3333333f;
         passed = expected == out;
         System.out.println(ruble + " rubles are 5 dollar. Test result : " + passed);
     }

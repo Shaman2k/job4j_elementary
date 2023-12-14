fca7d2759ad5bb65f96a27368e14cee28ba44925
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2023-12-14 11:43:48 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741]

---
+++ src/main/java/ru/job4j/array/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] result = new int[100];
+        int size = 0;
+
+        money -= price;
+        for (int i = 0; i < coins.length; i++) {
+            while (money - coins[i] >= 0) {
+                money -= coins[i];
+                result[size++] = coins[i];
+            }
+        }
+
+        return Arrays.copyOf(result, size);
+    }
+}
